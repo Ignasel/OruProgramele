460dabbbb111f19cbdaa3f9ae5869ed84018c2e0
Sarunas <sarunas.zube@gmail.com>
2019-12-05 09:45:58 +0200

    Su API pradžia

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+async function getData(city){
+    let url = 'https://api.meteo.lt/v1/places/'+city+'/forecasts/long-term';
+    let response = await fetch(url);
+    return await response.json();
+
+}
+
+
+async function showData() {
+    const data = await getData('Kaunas');
+    console.log(data['forecastTimestamps'])
+    for (let rows of data['forecastTimestamps']) {
+        console.log(rows)
+
+    }
+}
+showData()
