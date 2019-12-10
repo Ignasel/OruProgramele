5e72b7da91764d4a3f202b76fad4b447d4e1c1f0
Sarunas <sarunas.zube@gmail.com>
2019-12-10 16:23:27 +0200

    Beveik užbaigtos valandos

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+/*const clear - giedra; =
+const isolated-clouds - mažai debesuota; =
+const scattered-clouds - debesuota su pragiedruliais; =
+const overcast - debesuota; =
+const light-rain - nedidelis lietus; =
+const moderate-rain - lietus; =
+const heavy-rain - smarkus lietus; =
+const sleet - šlapdriba; =
+const light-snow - nedidelis sniegas; =
+const moderate-snow - sniegas; =
+const heavy-snow - smarkus sniegas; =
+const fog - rūkas; =
+const na - oro sąlygos nenustatytos. =
+const windArrow =
+*/
 
 
-async function getData(city){
-    let url = 'https://api.meteo.lt/v1/places/'+city+'/forecasts/long-term';
+
+
+
+async function getData(city) {
+    let url = 'https://api.meteo.lt/v1/places/' + city + '/forecasts/long-term';
     let response = await fetch(url);
     return await response.json();
 }
 
+
 async function showData() {
     const data = await getData('Kaunas');
-    let date = new Date(data['forecastTimestamps'][0]['forecastTimeUtc']).getTime();
-   // - ilgis masyvo console.log(data['forecastTimestamps'].length)
-   //  console.log(date);
-   //  console.log(data.forecastTimestamps[3])
+
 
     let initialTime = new Date(data['forecastTimestamps'][0]['forecastTimeUtc']);
     let day = initialTime.getDate();
@@ -21,114 +37,59 @@ async function showData() {
     let weatherData = [];
     const hours = document.querySelector(".hours")
     console.log(data.forecastTimestamps[0].forecastTimeUtc)
-    for(let i=0; i<24; i++) {
+    for (let i = 0; i < 24; i++)  {
         weatherData [i] = data.forecastTimestamps[i];
+
+        //------------Reikalingi kintamieji-------------------//
+
+        let newHour = new Date(data.forecastTimestamps[i].forecastTimeUtc);
 
         //---------------kuriu stulpelį--------------------------//
 
-        const whatHour = document.createElement("div")
-        hours.appendChild(whatHour)
-        whatHour.className = "col-1 hour";
+        const whatHour = document.createElement("div");
+        hours.appendChild(whatHour);
+        whatHour.className = "col hour";
+
+        //-------------------Pridedu valandas------//
 
         let time = document.createElement('div');
         whatHour.appendChild(time);
         time.className = 'row time';
-        time.innerText = data.forecastTimestamps[i].forecastTimeUtc
-        let temp = document.createElement("div")
-        whatHour.appendChild(temp)
-        temp.className = "row temperature"
-        temp.innerText = data.forecastTimestamps[i].airTemperature
+        time.innerText = newHour.getHours() +":00";
+
+        //---------------Pridedu condition ikoną-- naudoti switch--------//
+
+
+
+        //--------------Pridedu laipsius-----------------//
+
+        let temp = document.createElement("div");
+        whatHour.appendChild(temp);
+        temp.className = "row temperature";
+        temp.innerText = data.forecastTimestamps[i].airTemperature +" °";
+
+        //-------------------Kritulių kiekis--------------------------//
+
+        let percipitation = document.createElement("div")
+        whatHour.appendChild(percipitation)
+        percipitation.className = "row howMuchRain"
+        percipitation.innerText = data.forecastTimestamps[i].totalPrecipitation +" mm"
+
+        //---------------------Vėjo stiprumas-----------------------------//
+        let wind = document.createElement("div")
+        whatHour.appendChild(wind)
+        wind.className = "row windSpeed"
+        percipitation.innerText = data.forecastTimestamps[i].windSpeed +" m/s"
+
+
+        //----------------------Vėjo kryptis-------------------------------//
+
+
+        https://iconify.design/icon-sets/wi/wind-direction-s.html -- Erick flowers (Google)
+
     }
-    console.log(weatherData.airTemperature)
-    for(weatherItem of weatherData){
-           for(ItemValue in weatherItem) {
-              // console.log(data.forecastTimestamps[weatherItem].airTemperature)
-           }
-           }
 
 
-
-    // for (let i = 0; i < data['forecastTimestamps'].length ; i++) {
-    //     console.log(data['forecastTimestamps'][i]['forecastTimeUtc'])
-    // }
-
-
-
-
-
-//     let weather;
-//     const fetchWeather = async () => {
-//         weather = await fetch(
-//             'https://api.meteo.lt/v1/places/kaunas/forecasts/long-term'
-//         ).then(res => res.json());
-//     };
-//     const showWeather = async () => {
-// // getting the weather data from api
-//         await fetchWeather();
-//         const weatherItems = weather.forecastTimestamps;
-//         const weatherUiItems = document.querySelector('ul');
-//         let weatherData = [];
-//         for(weatherItem in weatherItems){
-//             weatherData [weatherItem] = weatherItems[weatherItem];
-// //console.log(weatherItems[weatherItem].forecastTimeUtc)
-//         }
-//         for(weatherItem of weatherData){
-//             for(ItemValue in weatherItem){
-//                 const li = document.createElement('li');
-//                 li.textContent = ItemValue+':'+weatherItem[ItemValue];
-//                 document.querySelector('ul').appendChild(li);
-//             }
-//         }
-//     }
-//     showWeather()
-//     }
-//
-//
-//     await showWeather()
-
-
-
-
-        // for (let hours of date['forecastTimestamps']) {
-        //     if (!dayTime) {
-        //         let hourTime = new Date(hour['forecastTimeUtc'])
-        //         dayTime = hourTime.getDate();
-        //         dayData = await filterDataByDate(data, dayTime);
-        //     }
-        // }
-
-    // let temperature = data['forecastTimestamps'][10]['airTemperature']
-    // console.log(temperature)
-    //
-    // for (let i=0; i<data['forecastTimestamps'].length; i++){
-    //     console.log(data['forecastTimestamps'][i]['airTemperature'])
-
-    //
-
-
-
-
-
-
-
-    // let tempNow = (data['forecastTimestamps'][10]['airTemperature']);
-    // console.log(tempNow)
-    //
-    // console.log(date)
-    // let dayData = data['forecastTimestamps'].filter(item => new Date(item['forecastTimeUtc']).getDate() === date);
-    // console.log(dayData)
-
-
-
-
-    // for (let hours of dayData){
-    //     console.log(hours)
-    // }
-
-
-    // for (let rows of data['forecastTimestamps']) {
-    //     console.log(rows['ForecastTimeUtc'])
-    //
-    // }
 }
+
 showData()
