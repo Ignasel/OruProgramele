3add7d21f436afc207e6cef4a5415c64ee51f7be
Sarunas <sarunas.zube@gmail.com>
2019-12-12 16:11:01 +0200

    Veikia valandos, filtruojama savaitė

--- main.js
+++ main.js
@@ -16,44 +16,29 @@ const windArrow =
 
 //------------------Visos galimos oro salygu ikonos-----------------------------//
 let weatherIcons = {
-    day:
-        {
-            clear: "wi-day-sunny",
-            isolatedClouds: "wi-day-sunny",
-            scatteredClouds: "wi-day-cloudy",
-            overcast:"wi-cloudy",
-            lightRain: "wi-day-sprinkle",
-            moderateRain: "wi-day-showers",
-            heavyRain: "wi-day-rain",
-            sleet: "wi-day-sleet",
-            lightSnow: "wi-day-snow",
-            moderateSnow: "wi-day-snow-wind",
-            heavySnow: "wi-day-snow-thunderstorm",
-            fog: "wi-day-fog"
-        },
-    night:
-        {
-            clear: "wi-night-clear",
-            isolatedClouds: "wi-night-alt-partly-cloudy",
-            scatteredClouds: "wi-night-alt-cloudy",
-            overcast: "wi-cloudy",
-            lightRain: "wi-night-alt-sprinkle",
-            moderateRain: "wi-night-alt-showers",
-            heavyRain: "wi-night-alt-rain",
-            sleet: "wi-night-alt-sleet",
-            lightSnow: "wi-night-alt-snow",
-            moderateSnow: "wi-night-alt-snow-wind",
-            heavySnow: "wi-night-alt-snow-thunderstorm",
-            fog: "wi-night-alt-fog"
-        },
-    other:
-        {
-            na: "wi-na",
-            degreeSymbol: "wi-degrees",
-            raindropSymbol: "wi-raindrops",
-            weatherIconClass: 'wi'
-        }
-};
+            clear: "wi wi-day-sunny",
+            isolatedClouds: "wi wi-day-sunny",
+            scatteredClouds: "wi wi-day-cloudy",
+            overcast: "wi wi-cloudy",
+            lightRain: "wi wi-day-sprinkle",
+            moderateRain: "wi wi-day-showers",
+            heavyRain: "wi wi-day-rain",
+            sleet: "wi wi-day-sleet",
+            lightSnow: "wi wi-day-snow",
+            moderateSnow: "wi wi-day-snow-wind",
+            heavySnow: "wi wi-day-snow-thunderstorm",
+            fog: "wi wi-day-fog"
+        };
+
+    // other:
+    //     {
+    //         na: "wi-na",
+    //         degreeSymbol: "wi-degrees",
+    //         raindropSymbol: "wi-raindrops",
+    //         weatherIconClass: 'wi'
+    //     }
+
+
 //----------------------pasiimu duomenis-----------------------//
 
 async function getData(city) {
@@ -66,19 +51,71 @@ async function getData(city) {
 async function showData() {
     const data = await getData('Kaunas');
     let initialTime = new Date(data['forecastTimestamps'][0]['forecastTimeUtc']);
-    let day = initialTime.getDate();
-    console.log(day)
+
+    async function weatherIcon(conditionCode) {
+        // let conditionCode = data.forecastTimestamps[i].conditionCode;
+        // if ((newHour.getHours() >6)&&(newHour.getHours() <21)) {
+        //      var timeOfDay = weatherIcons.day}
+        //     else { var timeOfDay = weatherIcons.night
+
+        switch (conditionCode) {
+            case ('clear'):
+                return weatherIcons.clear;
+            case ('isolated-clouds'):
+                return weatherIcons.isolatedClouds;
+            case ('scattered-clouds'):
+                return weatherIcons.scatteredClouds;
+            case ('overcast'):
+                return weatherIcons.overcast;
+            case ('light-rain'):
+                return weatherIcons.lightRain;
+            case ('moderate-rain'):
+                return weatherIcons.moderateRain;
+            case ('heavy-rain'):
+                return weatherIcons.heavyRain;
+            case ('sleet'):
+                return weatherIcons.sleet;
+            case ('light-snow'):
+                return weatherIcons.lightSnow;
+            case ('moderate-snow'):
+                return weatherIcons.moderateSnow;
+            case ('heavy-snow'):
+                return weatherIcons.heavySnow;
+            case ('fog'):
+                return weatherIcons.fog;
+            // default:
+            //     return weatherIcons.other.na;
+        }
+    }
+
+
+    //--------------išsiivesti septynias dienas--------//
+    console.log(data['forecastTimestamps'].length)
+     let allWeatherData = [];
+     for (let i = 0; i<data['forecastTimestamps'].length; i++) {
+         let newHour = new Date(data.forecastTimestamps[i].forecastTimeUtc);
+         allWeatherData[i] = data.forecastTimestamps[i];
+         let day = newHour.getDate();
+         console.log(day);
+
+         // newHour.getDate() = day1
+
+
+
+
+     }
+
+
 
     //-------išsivedu konkrečia temperatūrą-------//
+    const hours = document.querySelector(".hours");
+    console.log(data.forecastTimestamps[0].forecastTimeUtc);
     let weatherData = [];
-    const hours = document.querySelector(".hours")
-    console.log(data.forecastTimestamps[0].forecastTimeUtc)
-    for (let i = 0; i < 24; i++)  {
-        weatherData [i] = data.forecastTimestamps[i];
+    for (let i = 0; i < 24; i++) {
+        weatherData[i] = data.forecastTimestamps[i];
 
         //------------Reikalingi kintamieji-------------------//
 
-        let newHour = new Date(data.forecastTimestamps[i].forecastTimeUtc);
 
         //---------------kuriu stulpelį--------------------------//
 
@@ -87,16 +124,21 @@ async function showData() {
         whatHour.className = "col hour";
 
         //-------------------Pridedu valandas------//
-
+        let newHour = new Date(data.forecastTimestamps[i].forecastTimeUtc);
         let time = document.createElement('div');
         whatHour.appendChild(time);
         time.className = 'row time';
-        time.innerText = newHour.getHours() +":00";
+        time.innerText = newHour.getHours() + ":00";
 
         //---------------Pridedu condition ikoną-- naudoti switch--------//
 
-        
-
+        let icons = document.createElement('div');
+        whatHour.appendChild(icons);
+        icons.className = "row icons";
+        let icon = document.createElement('i');
+        icons.appendChild(icon);
+        icon.className = await weatherIcon(weatherData[i]['conditionCode'])
+        // icon.className = weatherIcon();
 
 
         //--------------Pridedu laipsius-----------------//
@@ -104,76 +146,35 @@ async function showData() {
         let temp = document.createElement("div");
         whatHour.appendChild(temp);
         temp.className = "row temperature";
-        temp.innerText = data.forecastTimestamps[i].airTemperature +" °";
+        temp.innerText = data.forecastTimestamps[i].airTemperature + " °";
 
         //-------------------Kritulių kiekis--------------------------//
 
 
-        let percipitation = document.createElement("div")
-        whatHour.appendChild(percipitation)
-        percipitation.className = "row howMuchRain"
-        percipitation.innerText = data.forecastTimestamps[i].totalPrecipitation +" mm"
+        let percipitation = document.createElement("div");
+        whatHour.appendChild(percipitation);
+        percipitation.className = "row howMuchRain";
+        percipitation.innerText = data.forecastTimestamps[i].totalPrecipitation + " mm";
 
         //---------------------Vėjo stiprumas-----------------------------//
-        let wind = document.createElement("div")
-        whatHour.appendChild(wind)
-        wind.className = "row windSpeed"
-        wind.innerText = data.forecastTimestamps[i].windSpeed +" m/s"
+        let wind = document.createElement("div");
+        whatHour.appendChild(wind);
+        wind.className = "row windSpeed";
+        wind.innerText = data.forecastTimestamps[i].windSpeed + " m/s";
 
 
         //----------------------Vėjo kryptis-------------------------------//
-        let windDirection = document.createElement("div")
-        whatHour.appendChild(windDirection)
-        wind.className = "row windDirection"
-        let windDirIcon  = document.createElement("i")
-        windDirection.appendChild(windDirIcon)
-        let degree = data.forecastTimestamps[i].windDirection
-        windDirIcon.className = "wi wi-wind from-" +degree+ "-deg"
+        let windDirection = document.createElement("div");
+        whatHour.appendChild(windDirection);
+        wind.className = "row windDirection";
+        let windDirIcon = document.createElement("i");
+        windDirection.appendChild(windDirIcon);
+        let degree = data.forecastTimestamps[i].windDirection;
+        windDirIcon.className = "wi wi-wind from-" + degree + "-deg"
 
-
-    }
-    
-    function weatherIcon() {
-
-            let icon = document.createElement('i');
-            let conditionCode = data.forecastTimestamps[i].conditionCode;
-
-
-            let timeOfDay = day ? weatherIcons.day : weatherIcons.night;
-            switch (conditionCode) {
-                case (conClear):
-                    return timeOfDay.clear;
-                case (conIsolatedClouds):
-                    return timeOfDay.isolatedClouds;
-                case (conScatteredClouds):
-                    return timeOfDay.scatteredClouds;
-                case (conOvercast):
-                    return timeOfDay.overcast;
-                case (conLightRain):
-                    return timeOfDay.lightRain;
-                case (conModerateRain):
-                    return timeOfDay.moderateRain;
-                case (conHeavyRain):
-                    return timeOfDay.heavyRain;
-                case (conSleet):
-                    return timeOfDay.sleet;
-                case (conLightSnow):
-                    return timeOfDay.lightSnow;
-                case (conModerateSnow):
-                    return timeOfDay.moderateSnow;
-                case (conHeavySnow):
-                    return timeOfDay.heavySnow;
-                case (conFog):
-                    return timeOfDay.fog;
-                default:
-                    return weatherIcons.other.na;
-            }
-        }
 
 
     }
-
-
 }
 
 showData()
